7e3876aee2083768be6164086db7642fd7714677
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-04-15 10:57:35 -0700

    prototypes of domain_wrap

--- domain.h
+++ domain.h
@@ -3,6 +3,8 @@ void domain_init(int Nmesh, double BoxSize);
 void domain_decompose(Particles* const particles, void * scratch, size_t scratch_bytes);
 void domain_decompose_min(Snapshot * const particles, void * scratch, size_t scratch_bytes);
 void domain_finalize();
+void domain_wrap();
+void domain_wrap_min();
 int domain_create_ghosts(Particles* const particles, double eps, void * scratch, size_t scratch_bytes);
 void domain_annihilate_ghosts(Particles* const particles, int nghosts, float (* f3)[3], 
         void * scratch, size_t scratch_bytes);
