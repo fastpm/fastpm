1f9a4a971690c30da9a0cf4ea953fe8dbff12be9
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-05-20 10:32:24 -0700

    fix warnings in msg.c and indents.

--- msg.c
+++ msg.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <mpi.h>
 #include "msg.h"
 
@@ -13,35 +14,35 @@ static int Log_level;
 // Initialize using msg_ functions.
 void msg_init()
 {
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 }
 
 void msg_set_loglevel(const enum LogLevel lv)
 {
-  Log_level= lv;
+    Log_level= lv;
 }
 
 void msg_printf(const enum LogLevel msg_level, const char *fmt, ...)
 {
-  if(myrank == 0 && msg_level >= Log_level) {
-    va_list argp;
+    if(myrank == 0 && msg_level >= Log_level) {
+        va_list argp;
 
-    va_start(argp, fmt);
-    vfprintf(stdout, fmt, argp);
-    fflush(stdout);
-    va_end(argp);
-  }
+        va_start(argp, fmt);
+        vfprintf(stdout, fmt, argp);
+        fflush(stdout);
+        va_end(argp);
+    }
 }
 
 void msg_abort(const int errret, const char *fmt, ...)
 {
-  va_list argp;
+    va_list argp;
 
-  va_start(argp, fmt);
-  vfprintf(stderr, fmt, argp);
-  va_end(argp);
+    va_start(argp, fmt);
+    vfprintf(stderr, fmt, argp);
+    va_end(argp);
 
     abort();
-  MPI_Abort(MPI_COMM_WORLD, errret);
+    MPI_Abort(MPI_COMM_WORLD, errret);
 }  
 
