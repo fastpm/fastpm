a96b3dadfe3dd0b429bd1707609db043d94aedf7
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-09-24 18:10:50 -0700

    Remove unused files inherited from cola_halo.

--- domain.h
+++
@@ -1,8 +0,0 @@
-
-void domain_init(double boxsize, int nc);
-void domain_set_size(int pm_nc_factor);
-void domain_decompose(Particles * particles);
-void domain_free();
-void domain_wrap(Particles * particles);
-int domain_create_ghosts(Particles * particles, double eps);
-void domain_annihilate_ghosts(Particles * particles, int nghosts, float (* f3)[3]);
