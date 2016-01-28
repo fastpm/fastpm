1a35eaa51d26d497e40382237e3508699f5c987d
Yu Feng <rainwoodman@gmail.com>
2016-01-27 16:29:08 -0800

    disable high resolution low noise kernels for grigor.

--- libfastpm/pmapi.c
+++ libfastpm/pmapi.c
@@ -138,6 +138,8 @@ pm_create_k_factors(PM * pm, PMKFactors * fac[3])
             fac[d][ind].kk = k * k;
             fac[d][ind].k = k;
             fac[d][ind].cic = ff;
+            fac[d][ind].k_finite = k;
+            fac[d][ind].kk_finite = k * k;
         }
     } 
 }
