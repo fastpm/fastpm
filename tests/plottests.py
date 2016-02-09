88e83de7078b12f962ba34252820f729037615b7
Yu Feng <rainwoodman@gmail.com>
2016-02-09 01:56:54 -0800

    plot correct power spectrumfiles.

--- tests/plottests.py
+++ tests/plottests.py
@@ -14,7 +14,7 @@ def plotps(filename, ax):
 
 fig = Figure(figsize=(12, 6))
 ax = fig.add_subplot(121)
-for fn in sorted(glob.glob('cola/powerspec*.txt')):
+for fn in sorted(glob.glob('cola/powerspec_*.txt')):
     plotps(fn, ax)
 ax.set_ylim(1, 1e5)
 ax.set_xscale('log')
@@ -22,7 +22,7 @@ ax.set_yscale('log')
 ax.set_title('cola: ' + TRAVIS_BUILD_NUMBER + ':' + TRAVIS_COMMIT)
 
 ax = fig.add_subplot(122)
-for fn in sorted(glob.glob('pm/powerspec*.txt')):
+for fn in sorted(glob.glob('pm/powerspec_*.txt')):
     plotps(fn, ax)
 
 ax.set_ylim(1, 1e5)
