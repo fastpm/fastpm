2d366b4d8702d59784dc5f90477e12350af6f92f
Yu Feng <yfeng1@berkeley.edu>
2016-02-18 14:47:50 -0800

    add some file checks to the lua stage.
    The default value handling shall be moved to lua too.

--- src/lua-preface.lua
+++ src/lua-preface.lua
@@ -32,11 +32,69 @@ function blendspace(start, e, a1, a2)
 end 
 
 function parse_file(filename)
-    env = setmetatable({}, {__index=_G})
-    param = loadfile(filename, 'bt', env)
+    local namespace = setmetatable({}, {__index=_G})
+    local param = loadfile(filename, 'bt', namespace)
     if param == nil then
         error(string.format("Could not open file %s", filename))
     end
     param()
-    return dump.tostring(env)
+    local required = {
+        nc = 'number',
+        boxsize = 'number',
+        time_step = 'table',
+        output_redshifts = 'table',
+        omega_m = 'number',
+        h = 'number',
+        pm_nc_factor = 'table',
+        change_pm = 'table',
+        np_alloc_factor = 'number',
+        force_mode = 'string',
+    }
+    local optional = {
+        read_grafic = 'string',
+        read_noisek = 'string',
+        read_noise = 'string',
+        read_runpbic = 'string',
+        read_powerspectrum = 'file',
+        read_sigma8 = 'number',
+        random_seed = 'number',
+        read_write_powerspectrum = 'string',
+        read_write_runpb_snapshot = 'string',
+        read_write_snapshot = 'string',
+        read_write_noisek = 'string',
+        read_write_noise = 'string',
+        cola_stdda = 'boolean',
+        enforce_broadband = 'boolean',
+        enforce_broadband_kmax = 'number',
+        za = 'boolean',
+    }
+    check_schema(namespace, required, true)
+    check_schema(namespace, optional, false)
+    return dump.tostring(namespace)
+end
+
+function check_schema(namespace, schema, required)
+    for key, t in pairs(schema) do
+        if namespace[key] == nil then
+            if required then
+                error(string.format("key `%s` is required but not defined", key))
+            end 
+        else
+            if t == 'file' then
+                t1 = 'string'
+            else
+                t1 = t
+            end
+            if type(namespace[key]) ~= t1 then
+                error(string.format("key `%s' is not of type `%s', but `%s' ", key, t1, type(namespace[t])))
+            end
+            if t == 'file' then
+                file = io.open(namespace[key], 'r')
+                if file == nil then
+                    error(string.format("file `%s' is not readable", namespace[key]))
+                end
+                file:close()
+            end
+        end
+    end 
 end
