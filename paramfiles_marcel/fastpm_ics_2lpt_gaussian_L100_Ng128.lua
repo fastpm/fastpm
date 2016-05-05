7b1c6713b412f4dc0db04d236eef161862561fd1
Marcel Schmittfull <marcel-sl@gmx.de>
2016-05-05 16:30:00 -0700

    Generated ICs with original FastPM code, using CAMB power spectrum generated on cosmos by Marcel.

---
+++ paramfiles_marcel/fastpm_ics_2lpt_gaussian_L100_Ng128.lua
@@ -0,0 +1,43 @@
+-- parameter file
+------ Size of the simulation -------- 
+
+-- For Testing
+nc = 128
+boxsize = 100.0
+
+-------- Time Sequence ----
+-- linspace: Uniform time steps in a
+-- time_step = linspace(0.025, 1.0, 39)
+-- logspace: Uniform time steps in loga
+-- time_step = linspace(0.01, 1.0, 10)
+time_step = {0.02}
+
+output_redshifts= {49.0}  -- redshifts of output
+
+-- Cosmology --
+omega_m = 0.29
+h       = 0.7
+
+-- Start with a power spectrum file
+-- Initial power spectrum: k P(k) in Mpc/h units
+-- Must be compatible with the Cosmology parameter
+read_powerspectrum = "planckDR2_5may2016b_matterpower.dat"
+random_seed = 100
+
+-------- Approximation Method ---------------
+force_mode = "pm"
+
+pm_nc_factor = {1,   2,   3, }            -- Particle Mesh grid pm_nc_factor*nc per dimension in the beginning
+change_pm =    {0, 0.2, 0.5, }            -- time(scaling factor) when the pm_nc_factor is changed, range from 0 to 1
+
+np_alloc_factor= 4.0      -- Amount of memory allocated for particle
+loglevel=0                 -- 0=verbose increase value to reduce output msgs
+
+-------- Output ---------------
+
+-- Dark matter particle outputs (all particles)
+write_runpb_snapshot= "ic/snp"       -- comment out to suppress snapshot output
+-- 1d power spectrum (raw), without shotnoise correction
+write_powerspectrum = "ic/powerspec"
+write_noisek = "ic/noisek"
+write_noise = "ic/noise"
