df2ad4e75f7bda79c6d977460739196e4c2c9234
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-05-23 10:57:50 -0700

    Add a stupid power spectrum function.

--- python/fastpm.py
+++ python/fastpm.py
@@ -65,3 +65,61 @@ class DumpFile(object):
             else:
                 size = numpy.maximum(size, last)
         return size
+
+def power(f1, f2=None, boxsize=1.0, return_modes=False):
+    """ stupid power spectrum calculator.
+
+        f1 f2 must be density fields in configuration or fourier space.
+
+        For convenience if f1 is strictly overdensity in fourier space,
+        (zero mode is zero) the code still works.
+
+        Returns k, p or k, p, N if return_modes is True
+
+    """
+    def tocomplex(f1):
+        if f1.dtype.kind == 'c':
+            return f1
+        else:
+            return numpy.rfftn(f1)
+    f1 = tocomplex(f1)
+    if f1[0, 0, 0] != 0.0:
+        f1 /= abs(f1[0, 0, 0])
+
+    if f2 is None:
+        f2 = f1
+
+    if f2 is not f1:
+        f2 = tocomplex(f2)
+        if f2[0, 0, 0] != 0.0:
+            f2 /= abs(f2[0, 0, 0])
+
+    def fftk(shape, boxsize, symmetric=True):
+        k = []
+        for d in range(len(shape)):
+            kd = numpy.fft.fftfreq(shape[d])
+            kd *= 2 * numpy.pi / boxsize * shape[d]
+            kdshape = numpy.ones(len(shape), dtype='int')
+            if symmetric and d == len(shape) -1:
+                kd = kd[:shape[d]//2 + 1]
+            kdshape[d] = len(kd)
+            kd = kd.reshape(kdshape)
+            k.append(kd)
+        kk = sum([i ** 2 for i in k])
+        return kk ** 0.5
+
+    x = (f1 * f2.conjugate()).real
+
+    k = fftk(f1.shape, boxsize, symmetric=False)
+    w = numpy.ones(k.shape, dtype='f4') * 2
+    w[:, :, 0] = 1
+    x *= w
+    edges = numpy.arange(f1.shape[-1]) * 2 * numpy.pi / boxsize
+    H, edges = numpy.histogram(k.flat, weights=x.flat, bins=edges)
+    N, edges = numpy.histogram(k.flat, weights=w.flat, bins=edges)
+    center = 0.5 * (edges[1:] + edges[:-1])
+    if return_modes:
+        return center, H / N *boxsize ** 3, N
+    else return_modes:
+        return center, H / N *boxsize ** 3
+
