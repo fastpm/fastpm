b2fe3d3c8cacf10a4947f5e03bc7042eac9f19fc
Yu Feng <rainwoodman@gmail.com>
2016-06-22 17:52:23 -0700

    update active test suites.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,22 +5,23 @@ source testfunctions.sh
 set -x
 mpirun -n 4 $FASTPM standard.lua ic || fail
 
+mpirun -n 4 $FASTPM standard.lua fastpm lineark || fail
+mpirun -n 4 $FASTPM standard.lua fastpm whitenoisek || fail
 mpirun -n 4 $FASTPM standard.lua fastpm || fail
-mpirun -n 4 $FASTPM standard.lua fastpm lanczos2 || fail
-mpirun -n 4 $FASTPM standard.lua fastpm lanczos3 || fail
-exit
-mpirun -n 4 $FASTPM standard.lua fastpm gaussian || fail
-mpirun -n 4 $FASTPM standard.lua fastpm aggressive || fail
-mpirun -n 4 $FASTPM standard.lua fastpm gadget || fail
-mpirun -n 4 $FASTPM standard.lua fastpm eastwood || fail
-mpirun -n 4 $FASTPM standard.lua fastpm eastwood gaussian|| fail
-mpirun -n 4 $FASTPM standard.lua fastpm twothird || fail
-mpirun -n 4 $FASTPM standard.lua fastpm gaussian36 || fail
+
+#mpirun -n 4 $FASTPM standard.lua fastpm lanczos2 || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm lanczos3 || fail
+
+#mpirun -n 4 $FASTPM standard.lua fastpm gaussian || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm aggressive || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm gadget || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm eastwood || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm eastwood gaussian|| fail
+#mpirun -n 4 $FASTPM standard.lua fastpm twothird || fail
+#mpirun -n 4 $FASTPM standard.lua fastpm gaussian36 || fail
 
 mpirun -n 4 $FASTPM standard.lua za fnl|| fail
 
-mpirun -n 4 $FASTPM standard.lua fastpm lineark || fail
-mpirun -n 4 $FASTPM standard.lua fastpm whitenoisek || fail
 
 mpirun -n 4 $FASTPM standard.lua za || fail
 mpirun -n 4 $FASTPM standard.lua 2lpt || fail
