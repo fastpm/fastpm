29726d74c01132e93e4a1c02079ad52de1da883e
Yu Feng <rainwoodman@gmail.com>
2016-09-29 15:55:28 -0700

    add gravity.h

---
+++ api/fastpm/gravity.h
@@ -0,0 +1,12 @@
+FASTPM_BEGIN_DECLS
+typedef struct {
+    FastPMKernelType KernelType;
+    FastPMDealiasingType DealiasingType;
+    FastPMPainterType PainterType;
+    int PainterSupport;
+} FastPMGravity;
+
+void
+fastpm_gravity_calculate(FastPMGravity * gravity, PM * pm, FastPMStore * p, FastPMFloat * delta_k);
+
+FASTPM_END_DECLS
