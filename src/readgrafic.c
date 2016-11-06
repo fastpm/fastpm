0fce943bd03a659c1dc6c7a31e78017826b35147
Yu Feng <rainwoodman@gmail.com>
2016-11-06 02:16:42 -0800

    more warnings.

--- src/readgrafic.c
+++ src/readgrafic.c
@@ -24,7 +24,9 @@ read_grafic_gaussian(PM * pm, FastPMFloat * g_x, char * filename)
         int32_t bs2;
     } header;
 
-    fread(&header, sizeof(header), 1, fp);
+    if(1 != fread(&header, sizeof(header), 1, fp)) {
+        fastpm_raise(-1, "file not in BigMD noise format.\n");
+    }
 
     ind = 0;
 
@@ -53,13 +55,17 @@ read_grafic_gaussian(PM * pm, FastPMFloat * g_x, char * filename)
 
         fseek(fp, offset, SEEK_SET);
 
-        fread(&bs, 4, 1, fp);
+        if(1 != fread(&bs, 4, 1, fp))
+            fastpm_raise(-1, "file not in BigMD noise format.\n");
 
         if(bs != 4 * header.n[0] * header.n[1])
             fastpm_raise(-1, "file size is wrong\n");
 
         fseek(fp, pm_i_region(pm)->start[1] * header.n[1] * 4, SEEK_CUR);
-        fread(buf, 4, header.n[0] * pm_i_region(pm)->size[1], fp);
+
+        if(header.n[0] * pm_i_region(pm)->size[1] != fread(buf, 4, header.n[0] * pm_i_region(pm)->size[1], fp))
+            fastpm_raise(-1, "file not in BigMD noise format.\n");
+
         int p = 0;
         for(i[1] = 0; i[1] < pm_i_region(pm)->size[1]; i[1] ++) {
             /* note that size[2] is Nmesh[2] == n[0] and start[2] is 0 */
