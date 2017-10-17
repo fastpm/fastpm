6152dd75de13df6992bd9c69b15034f2f59f62fd
Yu Feng <rainwoodman@gmail.com>
2017-10-17 14:48:59 -0700

    more features in fof.py

--- python/fof.py
+++ python/fof.py
@@ -10,7 +10,8 @@ def main():
     ap = ArgumentParser()
     ap.add_argument('fpm', help='e.g. /scratch/fpm_0.1000/')
     ap.add_argument('ll', type=float, help='e.g. 0.2 or 0.168') 
-    ap.add_argument('fof', help='e.g. /scratch/fpm_0.1000_fof/0.168')
+    ap.add_argument('--with-peak', help='Find Peaks KDDensity estimation (slow)', default=True)
+    ap.add_argument('fof', help='e.g. /scratch/fpm_0.1000/fof . Will write to {fof}/{ll}')
     ap.add_argument('--nmin', type=int, default=20, help='min number of particles to be in the catalogue')
 
     ns = ap.parse_args()
@@ -20,7 +21,7 @@ def main():
     cat.attrs['BoxSize']  = numpy.ones(3) * cat.attrs['BoxSize'][0]
     cat.attrs['Nmesh']  = numpy.ones(3) * cat.attrs['NC'][0]
 
-    cosmo = Planck15.clone(Om0=cat.attrs['OmegaM'])
+    cosmo = Planck15.match(Omega0_m=cat.attrs['OmegaM'])
 
     M0 = cat.attrs['OmegaM'][0] * 27.75 * 1e10 * cat.attrs['BoxSize'].prod() / cat.csize
 
@@ -31,11 +32,16 @@ def main():
         print('OmegaM', cosmo.Om0)
 
 
-    cat['Density'] = KDDensity(cat).density
+    if ns.with_peak:
+        cat['Density'] = KDDensity(cat).density
 
     fof = FOF(cat, linking_length=ns.ll, nmin=ns.nmin)
 
-    features = fof.find_features(peakcolumn='Density')
+    if ns.with_peak:
+        features = fof.find_features(peakcolumn='Density')
+    else:
+        features = fof.find_features(peakcolumn=None)
+
     features['Mass'] = M0 * features['Length']
     if fof.comm.rank == 0:
         print('Total number of features found', features.csize)
