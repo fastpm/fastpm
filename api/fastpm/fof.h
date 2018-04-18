0366a7fea106bfd59632b4c44ea7e6d787762ce6
Yu Feng <rainwoodman@gmail.com>
2018-04-18 10:25:28 -0700

    missing fof.h

---
+++ api/fastpm/fof.h
@@ -0,0 +1,28 @@
+#ifndef _FASTPM_FOF_H
+#define _FASTPM_FOF_H
+typedef struct FastPMFOFFinderPrivate FastPMFOFFinderPrivate;
+
+typedef struct {
+    int nmin;
+    double linkinglength; /* absolute */
+    FastPMStore halos[1];
+
+    FastPMStore * p;
+    PM * pm;
+
+    /* private */
+    uint64_t * label;
+
+    FastPMFOFFinderPrivate * priv;
+
+} FastPMFOFFinder;
+
+void
+fastpm_fof_init(FastPMFOFFinder * finder, FastPMStore * store, PM * pm);
+
+void
+fastpm_fof_execute(FastPMFOFFinder * finder, FastPMStore * halos);
+
+void
+fastpm_fof_destroy(FastPMFOFFinder * finder);
+#endif
