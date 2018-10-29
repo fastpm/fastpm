d0702abd99cd31bf1a3b8218d89274a67b111225
Yu Feng <rainwoodman@gmail.com>
2018-10-29 16:31:32 -0700

    Add a standalone halo finder, fastpm-fof.
    It only works with a fastpm snapshot where
    the evaluated parameters are stored in the header.

---
+++ src/fastpm-fof.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <alloca.h>
+#include <mpi.h>
+#include <math.h>
+#include <signal.h>
+#include <getopt.h>
+#include <limits.h>
+
+#include <fastpm/libfastpm.h>
+#include <fastpm/prof.h>
+#include <fastpm/logging.h>
+#include <fastpm/string.h>
+#include <fastpm/io.h>
+#include <fastpm/fof.h>
+#include <bigfile.h>
+#include <bigfile-mpi.h>
+
+#include "lua-config.h"
+#include "param.h"
+
+extern void
+init_stacktrace();
+
+LUAParameters * read_lua_parameters_mpi(const char * filebase, char ** error, MPI_Comm comm)
+{
+    *error = NULL;
+    LUAParameters * prr = malloc(sizeof(prr[0]));
+
+    BigFile bf[1];
+    BigBlock bb[1];
+    BigAttr * attr;
+
+    big_file_mpi_open(bf, filebase, comm);
+
+    big_file_mpi_open_block(bf, bb, "Header", comm);
+
+    attr = big_block_lookup_attr(bb, "ParamFile");
+
+    char * confstr = malloc(attr->nmemb + 1);
+    big_block_get_attr(bb, "ParamFile", confstr, "S1", attr->nmemb);
+
+    prr->config = lua_config_new(confstr);
+
+    if(lua_config_error(prr->config)) {
+        *error = strdup(lua_config_error(prr->config));
+        free(prr);
+        return NULL;
+    }
+
+    prr->string = confstr;
+
+    big_block_mpi_close(bb, comm);
+    big_file_mpi_close(bf, comm);
+    return prr;
+}
+
+int
+main(int argc, char * argv[])
+{
+    init_stacktrace();
+
+    MPI_Init(&argc, &argv);
+
+    libfastpm_init();
+
+    MPI_Comm comm = MPI_COMM_WORLD;
+
+    fastpm_set_msg_handler(fastpm_default_msg_handler, comm, NULL);
+    fastpm_info("This is FastPM, with libfastpm version %s.\n", LIBFASTPM_VERSION);
+
+    char * error;
+
+    CLIParameters * cli = parse_cli_args_mpi(argc, argv, comm);
+    if(cli->argc < 2) {
+        fastpm_raise(-1, "Must supply a snapshot file name and a linking length\n");
+    }
+    char * filebase = cli->argv[0];
+    double linkinglength = atof(cli->argv[1]);
+    char * dataset = fastpm_strdup_printf("LL-%05.3f", linkinglength);
+
+    fastpm_info("Running FOF on %s; writing to %s\n", filebase, dataset);
+    LUAParameters * lua = read_lua_parameters_mpi(filebase, &error, comm);
+
+    if(lua) {
+        fastpm_info("Configuration %s\n", lua->string);
+    } else {
+        fastpm_info("Parsing configuration failed with error: %s\n", error);
+        MPI_Finalize();
+        exit(1);
+    }
+
+    libfastpm_set_memory_bound(cli->MemoryPerRank * 1024 * 1024);
+
+    FastPMStore snapshot[1];
+
+    fastpm_store_init_evenly(snapshot, pow(1.0 * CONF(lua, nc), 3),
+          COLUMN_POS | COLUMN_VEL | COLUMN_ID | COLUMN_MASK | COLUMN_ACC,
+        CONF(lua, np_alloc_factor), comm);
+
+    PM * basepm = fastpm_create_pm(CONF(lua, nc), cli->NprocY, 1, CONF(lua, boxsize), comm);
+
+    fastpm_store_write(snapshot, filebase, "1", "r", cli->Nwriters, comm);
+
+    CLOCK(fof);
+    CLOCK(io);
+    CLOCK(sort);
+    ENTER(fof);
+    FastPMFOFFinder fof = {
+        /* convert from fraction of mean separation to simulation distance units. */
+        .linkinglength = linkinglength * CONF(lua, boxsize) / CONF(lua, nc),
+        .periodic = 1,
+        .nmin = CONF(lua, fof_nmin),
+        .kdtree_thresh = CONF(lua, fof_kdtree_thresh),
+    };
+
+    fastpm_fof_init(&fof, snapshot, basepm);
+
+    FastPMStore halos[1];
+
+    ENTER(fof);
+
+    fastpm_fof_execute(&fof, halos);
+
+    LEAVE(fof);
+
+    ENTER(sort);
+    fastpm_sort_snapshot(halos, comm, FastPMSnapshotSortByLength, 0);
+    LEAVE(sort);
+
+    ENTER(io);
+    fastpm_store_write(halos, filebase, dataset, "w", cli->Nwriters, comm);
+
+    LEAVE(io);
+
+    fastpm_store_destroy(halos);
+    fastpm_fof_destroy(&fof);
+    free_lua_parameters(lua);
+    free_cli_parameters(cli);
+
+    fastpm_store_destroy(snapshot);
+    fastpm_free_pm(basepm);
+
+    free(dataset);
+    fastpm_clock_stat(comm);
+
+    libfastpm_cleanup();
+
+    MPI_Finalize();
+
+    return 0;
+
+}
