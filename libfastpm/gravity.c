680b3f3dfb262cc832c408e5f3afba5f300fa68a
Yu Feng <rainwoodman@gmail.com>
2019-02-01 13:57:04 -0800

    Send the mass of particles.
    Because painting requires mass, if we don't sent it the
    painting of ghosts is wrong.

--- libfastpm/gravity.c
+++ libfastpm/gravity.c
@@ -269,6 +269,9 @@ _fastpm_solver_create_ghosts(FastPMSolver * fastpm, int support, PMGhostData * p
         if(!p) continue;
         pgd[si] = pm_ghosts_create(pm, p, p->attributes, support);
         pm_ghosts_send(pgd[si], COLUMN_POS);
+        pm_ghosts_send(pgd[si], COLUMN_ID);
+        if(p->mass)
+            pm_ghosts_send(pgd[si], COLUMN_MASS);
     }
     LEAVE(ghosts);
 }
@@ -314,16 +317,19 @@ _fastpm_solver_compute_delta_k(FastPMSolver * fastpm, FastPMPainter * painter, P
 
         VALGRIND_CHECK_MEM_IS_DEFINED(p->x, sizeof(p->x[0]) * p->np);
         VALGRIND_CHECK_MEM_IS_DEFINED(pgd[si]->p->x, sizeof(pgd[si]->p->x[0]) * pgd[si]->p->np);
-        
-        int i;
-        for (i = 0; i <= p->np; i ++){
-            total_mass += fastpm_store_get_mass(p, i);
+
+        double total_mass1 = 0;
+        ptrdiff_t i;
+        for (i = 0; i < p->np; i ++){
+            total_mass1 += fastpm_store_get_mass(p, i);
         }
+        total_mass += total_mass1;
         fastpm_paint_local(painter, canvas, p, p->np, FASTPM_FIELD_DESCR_NONE);
         fastpm_paint_local(painter, canvas, pgd[si]->p, pgd[si]->p->np, FASTPM_FIELD_DESCR_NONE);
     }
     LEAVE(paint);
 
+    MPI_Allreduce(MPI_IN_PLACE, &total_mass, 1, MPI_DOUBLE, MPI_SUM, fastpm->comm);
     double mean_mass_per_cell = total_mass / pm->Norm;
 
     CLOCK(transfer);
