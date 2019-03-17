41b39cd6b522ab5aeab10994bfbff6d1d748722f
Adrian Bayer <abayer@berkeley.edu>
2019-03-16 21:16:51 -0700

    updated para file

--- tests/ncdm.lua
+++ tests/ncdm.lua
@@ -1,18 +1,14 @@
 -- parameter file
 ------ Size of the simulation -------- 
-
+-- ncdm.lua n_shell n_side n_p
 -- For Testing
 nc = 128
-boxsize = 1024
+boxsize = 1000
 
 -------- Time Sequence ----
 -- linspace: Uniform time steps in a
--- time_step = linspace(0.025, 1.0, 39)
 -- logspace: Uniform time steps in loga
--- time_step = linspace(0.01, 1.0, 10)
---time_step = linspace(0.1, 1, 10)
---time_step = linspace(0.01, 0.02, 10)
-time_step = linspace(0.01, 0.02, 10)
+time_step = linspace(0.01,0.02,20)
 
 output_redshifts= {99.0, 49.}  -- redshifts of output
 
@@ -22,19 +18,21 @@ omega_ncdm = 0.001404
 h       = 0.6774
 
 --ncdm split
-m_ncdm = {0.15}
+m_ncdm = {0.05,0.05,0.05}
 n_shell = 10
 n_side = 1
-lvk = false
+lvk = true
 every_ncdm = 1
 
 -- Start with a linear density field
 -- Power spectrum of the linear density field: k P(k) in Mpc/h units
 -- Must be compatible with the Cosmology parameter
-read_powerspectrum= "powerspec.txt"
-linear_density_redshift = 0.0 -- the redshift of the linear density field.
-random_seed= 100
+read_powerspectrum= "Pcb.txt"
+linear_density_redshift = 99.0 -- the redshift of the linear density field.
+random_seed= 42
 particle_fraction = 1.0
+
+--sort_snapshot = false
 --
 -------- Approximation Method ---------------
 force_mode = "fastpm"
@@ -43,11 +41,19 @@ pm_nc_factor = 2
 
 np_alloc_factor= 4.0      -- Amount of memory allocated for particle
 
+remove_cosmic_variance = true
+
+dealiasing_type = "aggressive"
+
 -------- Output ---------------
 
+filename = string.format("srunE_SH%d_NS%d_every%d_proc%d_size%d_lvk%s_rcv%s_dt%s_B0.1", n_shell, n_side, every_ncdm, os.get_nprocs(), nc, lvk, remove_cosmic_variance, dealiasing_type)
+--loc = "/global/cscratch1/sd/abayer/fastpm/ncdm/Pncdm_init_test/"
+loc = "/global/cscratch1/sd/abayer/trash/"--fastpm/ncdm/Pncdm_init_test/"
+
 -- Dark matter particle outputs (all particles)
-write_snapshot= "ncdm/fastpm" 
+write_snapshot = loc .. filename .. "/fastpm" 
 -- 1d power spectrum (raw), without shotnoise correction
-write_powerspectrum = "ncdm/powerspec"
--- write_fof = "ncdm/fastpm"
+write_powerspectrum = loc .. filename .. "/powerspec"
+--write_fof = loc .. filename .. "/fastpm"
 
