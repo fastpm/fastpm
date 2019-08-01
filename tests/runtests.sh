5ed2f294877516376d65c558ec4e67498f426c3a
Yu Feng <rainwoodman@gmail.com>
2019-07-31 20:06:37 -0700

    Add a test case for the nbodykit.lua file (#59)
    * Add a test case for the nbodykit.lu file.
    
    Checks the number of halos formed at z=0.5 and z=0.0, also
    checks the numerical values of sigma8 at z=0.0 and RSD factor
    at z=0.5.
    
    This should guard against regressions on the most common use case
    and discover changes in the background equation.
    
    * Enable nbodykit test in travis.
    
    * remove extra duplicated line.
    
    * Add better testing util functions.
    
    Also fix the filenaming issue in run-test-nbodykit.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,36 +1,34 @@
-#! /bin/bash
+#assert_success "! /bin/bash
 
 source testfunctions.sh
 
 FASTPM="`dirname $0`/../src/fastpm -T 1"
 
-set -x
+assert_success "mpirun -n 4 $FASTPM standard.lua ic"
+assert_success "mpirun -n 4 $FASTPM standard.lua fastpm lineark"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm whitenoisek"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm"
 
-mpirun -n 4 $FASTPM standard.lua ic || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm lineark || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm whitenoisek || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm || fail
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm lanczos2"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm lanczos3"
 
-#mpirun -n 4 $FASTPM standard.lua fastpm lanczos2 || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm lanczos3 || fail
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm gaussian"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm aggressive"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm gadget"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm eastwood"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm eastwood gaussian"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm twothird"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm gaussian36"
 
-#mpirun -n 4 $FASTPM standard.lua fastpm gaussian || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm aggressive || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm gadget || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm eastwood || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm eastwood gaussian|| fail
-#mpirun -n 4 $FASTPM standard.lua fastpm twothird || fail
-#mpirun -n 4 $FASTPM standard.lua fastpm gaussian36 || fail
-
-mpirun -n 4 $FASTPM standard.lua za fnl|| fail
+assert_success "mpirun -n 4 $FASTPM standard.lua za fnl"
 
 
-mpirun -n 4 $FASTPM standard.lua za || fail
-#mpirun -n 4 $FASTPM standard.lua 2lpt || fail
-mpirun -n 4 $FASTPM standard.lua fastpm || fail
-#mpirun -n 4 $FASTPM standard.lua pm || fail
-#mpirun -n 4 $FASTPM standard.lua cola || fail
+assert_success "mpirun -n 4 $FASTPM standard.lua za"
+#assert_success "mpirun -n 4 $FASTPM standard.lua 2lpt"
+assert_success "mpirun -n 4 $FASTPM standard.lua fastpm"
+#assert_success "mpirun -n 4 $FASTPM standard.lua pm"
+#assert_success "mpirun -n 4 $FASTPM standard.lua cola"
 
-# mpirun -n 4 $FASTPM standard.lua fastpm inverted || fail
-# mpirun -n 4 $FASTPM standard.lua fastpm remove_variance || fail
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm inverted"
+#assert_success "mpirun -n 4 $FASTPM standard.lua fastpm remove_variance"
 
--- tests/testfunctions.sh
+++ tests/testfunctions.sh
@@ -1,3 +1,45 @@
-fail () {
+die () {
     exit 1
+}
+
+NUMFAILS=0
+
+# assert command $1 returns 0.
+assert_success() {
+  local source=$0
+  local cmd=$1
+  echo "$source:" "$cmd"
+  ( eval $cmd )
+  if [ $? -eq 0 ]; then
+     echo "[ OK   ]" "$source:" "$cmd"
+     return 0
+  else
+     echo "[ FAIL ]" "$source:" "$cmd"
+     NUMFAILS=$(($NUMFAILS + 1))
+     return 1
+  fi
+
+}
+# look for $2 in the file $1, die if not found.
+assert_file_contains () {
+  local source=$0
+  local log=$1
+  local pattern=$2
+  if grep -q "$pattern" "$log" ; then
+     echo "[ OK   ]" "$source:" Regex pattern "$pattern" matched by file "$log".
+     return 0
+  else
+     echo "[ FAIL ]" "$source:" Regex pattern "$pattern" not matched by file "$log".
+     NUMFAILS=$(($NUMFAILS + 1))
+     return 1
+  fi
+}
+
+report_test_status () {
+  if [ $NUMFAILS -gt 0 ]; then
+     echo "$0: Some tests have failed."
+     die
+  else
+     echo "$0: All tests have passed."
+  fi
 }
