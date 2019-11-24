1a83200f96d424ff9bdfe6b96901f015ab115a28
Yu Feng <rainwoodman@gmail.com>
2019-11-23 20:15:37 -0800

    Update down-integrate.sh

--- depends/mpsort/down-integrate.sh
+++ depends/mpsort/down-integrate.sh
@@ -1,5 +1,16 @@
-tmp=$(mktemp -d)
-trap "rm -rf $tmp" EXIT
+#
+# Run this script to download MP-Sort source code.
+#
 
+tmp=$(mktemp -d)
+if ! git diff --cached --exit-code; then
+  echo "There are changes in the index. Commit or reset first."
+  exit 1
+fi
+
+trap "rm -rf $tmp" EXIT
 git clone https://github.com/rainwoodman/MP-Sort $tmp
+sha=$(cd $tmp; git rev-parse --verify HEAD)
 rsync -avz $tmp/* .
+git add *.[ch] Makefile
+git commit -m "Update mp-sort to $sha"
