e2a59821dfa76ff885217b0f70b9c404d29d3518
Yu Feng <rainwoodman@gmail.com>
2019-11-24 17:38:42 -0800

    Add script to update kdcount.

---
+++ depends/kdcount/down-integrate.sh
@@ -0,0 +1,16 @@
+#
+# Run this script to download MP-Sort source code.
+#
+
+tmp=$(mktemp -d)
+if ! git diff --cached --exit-code; then
+  echo "There are changes in the index. Commit or reset first."
+  exit 1
+fi
+
+trap "rm -rf $tmp" EXIT
+git clone https://github.com/rainwoodman/kdcount $tmp
+sha=$(cd $tmp; git rev-parse --verify HEAD)
+rsync -avz -f '- /*/' -f '- *.py' -f '- *.pyx' $tmp/kdcount/* .
+git add *.[ch] Makefile
+git commit -m "Update kdcount to $sha"
