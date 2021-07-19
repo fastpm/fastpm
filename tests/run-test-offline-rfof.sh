35a33cec29abea87634a6d8baad4f983edc83921
Yu Feng <rainwoodman@gmail.com>
2021-07-18 17:32:35 -0700

    Add integration tests for rfof.

---
+++ tests/run-test-offline-rfof.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+source testfunctions.sh
+
+FASTPM="`dirname $0`/../src/fastpm -T 1"
+RFOF="`dirname $0`/../src/fastpm-rfof -T 1"
+log=`mktemp`
+
+assert_success "mpirun -n 4 $FASTPM restart.lua > $log"
+
+echo "---- Validating the log output -------"
+assert_file_contains $log 'Velocity dispersion (a = 0.6124): std = 1.63807 1.75754 1.94999'
+assert_file_contains $log 'Velocity dispersion (a = 0.8660): std = 2.44703 2.62561 2.90857'
+assert_file_contains $log 'Writing 4017 objects'
+
+log=`mktemp`
+assert_success "mpirun -n 4 $RFOF restart/fastpm_1.0000 1.0 > $log"
+echo "---- Validating the log output -------"
+assert_file_contains $log 'Writing 4017 objects'
+
+report_test_status
