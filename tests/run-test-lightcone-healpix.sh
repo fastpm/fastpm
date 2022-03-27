a917be0239970d3dc40e7d865343b32018cea232
Yu Feng <rainwoodman@gmail.com>
2022-03-26 20:55:22 -0700

    Add a test for healpix maps in lc.
    (Need to confirm if these numbers actually are sane)

---
+++ tests/run-test-lightcone-healpix.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+source testfunctions.sh
+
+FASTPM="`dirname $0`/../src/fastpm -T 1"
+log=`mktemp`
+check=run-test-lightcone-healpix.check
+
+assert_success "mpirun -n 4 $FASTPM lightcone-healpix.lua > $log"
+
+echo "---- Validating the log output $log with $check-------"
+assert_success "cat $log | filecheck $check"
